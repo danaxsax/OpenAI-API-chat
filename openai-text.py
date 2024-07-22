5e412ebc6b7cd2fa9d0ea3d8af61d1e2693afe6a
danaxsax <dana.salinas.tareas@gmail.com>
2024-07-22 10:30:31 -0600

    feat: intial

--- openai-text.py
+++ openai-text.py
@@ -1,13 +1,11 @@
 from openai import OpenAI
 client = OpenAI()
 
-
 system_content = input("Introduce el contexto delsistema: ")
 user_content = input("Introduce el prompt del usuario: ")
 
-
 completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
+  model="gpt-4o-mini",
   messages=[
     {"role": "system", "content": system_content},
     {"role": "user", "content": user_content}
